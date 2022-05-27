19de06092f165b4f73cb6fca9cbf7d5d773118f0
Juliano Souza <souzza.s1990@gmail.com>
2022-05-27 14:41:00 -0300

    saving feedback in sqlite with prism

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 
 const app = express();
+app.use(express.json() );
 
-app.get('/users', (req, res) => {
- res.send('olá mundo')
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body; 
+
+  const feedback = await prisma.feedback.create({
+    data: { 
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+
+  res.status(201).json({data: feedback});
 })
 
 app.listen(3333, function(){
